8e91994f7a4a423a46dc6adec9e4c8914238f702
FortifyDIY <33672303+FortifyDIY@users.noreply.github.com>
2018-01-04 22:46:55 -0500

    1
    1

--- Check-KBinstall.ps1
+++ Check-KBinstall.ps1
@@ -1,22 +1,49 @@
 ﻿$BuildNo = [System.Environment]::OSVersion.Version.Build
-
+$applied = 'Updates applied'
+$notFound = 'Hotfix not found on this system. Please run Windows Update.'
 
 if ($BuildNo -eq '16299') {
      get-hotfix | where {$_.HotFixId -like "KB4056892"}
+     write-host -f Green $applied
+     exit
+     }
+     else {
+     write-host -f Red $notFound
      }
      elseif ($BuildNo -eq '15063') {
+     
+if ($BuildNo -eq '15063') {
      get-hotfix | where {$_.HotFixId -like "KB4056891"}
+     write-host -f Green $applied
+     exit
      }
-     elseif ($BuildNo -eq '14393') {
-     get-hotfix | where {$_.HotFixId -like "KB4056890"}
-     }
-     elseif ($BuildNo -eq '10586') {
-     get-hotfix | where {$_.HotFixId -like "KB4056888"}
-     }
-     elseif ($BuildNo -eq '10240') {
-     get-hotfix | where {$_.HotFixId -like "KB4056893"}
-     } 
      else {
-     write-host -f Red 'Hotfix not found on this system. Please run Windows Update.'
+     write-host -f Red $notFound
      }
-    
+
+if ($BuildNo -eq '14393') {
+     get-hotfix | where {$_.HotFixId -like "KB4056890"}
+     write-host -f Green $applied
+     exit
+     }
+     else {
+     write-host -f Red $notFound
+     }
+
+if ($BuildNo -eq '10586') {
+     get-hotfix | where {$_.HotFixId -like "KB4056888"}
+     write-host -f Green $applied
+     exit
+     }
+     else {
+     write-host -f Red $notFound
+     }
+
+if ($BuildNo -eq '10240') {
+     get-hotfix | where {$_.HotFixId -like "KB4056893"}
+     write-host -f Green $applied
+     exit
+     }
+     else {
+     write-host -f Red $notFound
+     }
